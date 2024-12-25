60b77b1fc0a9adf5599488999618d0e1df724469
saikumarseepana <saikumarseepana436@gmail.com>
2024-12-25 10:59:06 +0530

    Palindrome (String)

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    public static void main(String[] args){
+        String str = "abnmjba";
+        System.out.println(isPalindrome(str));
+
+    }
+
+    static boolean isPalindrome(String str){
+        str = str.toLowerCase();
+        int start = 0;
+        int end = str.length() - 1;
+        while(start <= end){
+            if(str.charAt(start) != str.charAt(end)){
+                return false;
+            }else{
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
