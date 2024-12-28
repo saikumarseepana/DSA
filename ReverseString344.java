68dfca95185e74b0100844fae23d53454a3f50bb
saikumarseepana <saikumarseepana436@gmail.com>
2024-12-28 16:03:03 +0530

    Reverse String 344 updated with 2 approaches(2-pointer and recursion)

--- ReverseString344.java
+++ ReverseString344.java
@@ -1,5 +1,8 @@
 class Solution {
     public void reverseString(char[] s) {
+        // reverse(s, 0, s.length - 1); *** recursive approach has O(n) space complexity where as 
+                                        //iterative two pointer approach has O(1)                        
+
         int start = 0;
         int end = s.length - 1;
         while(start <= end){
@@ -14,4 +17,18 @@ class Solution {
         s[first] = s[second];
         s[second] = temp;
     }
+
+    // static void reverse(char[] s, int start, int end){ // recursion approach
+    //     if(start >= end){
+    //         return;
+    //     }
+
+    //     char temp = s[start];
+    //     s[start] = s[end];
+    //     s[end] = temp;
+
+    //     reverse(s, start + 1, end - 1);
+    // }
+
+    
 }
