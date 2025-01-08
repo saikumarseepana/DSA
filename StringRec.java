c56bf320f525941a1e9610a0d083b87280143bed
saikumarseepana <saikumarseepana436@gmail.com>
2025-01-08 09:07:19 +0530

    Skipping a character in String (Recursion)

---
+++ StringRec.java
@@ -0,0 +1,34 @@
+public class StringRec {
+    public static void main(String[] args){
+        System.out.println(skip1("baccdaha"));
+    }
+
+    static void skip(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skip(p, up.substring(1));
+        }else{
+            skip(p + ch, up.substring(1));
+        }
+
+    }
+
+    static String skip1(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            return skip1(up.substring(1));
+        }else{
+            return ch + skip1(up.substring(1));
+        }
+
+    }
+}
