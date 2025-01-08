f4b5d5f23438ea9ac7676b0269bc2d2dcd443699
saikumarseepana <saikumarseepana436@gmail.com>
2025-01-08 09:18:17 +0530

    added extra cases for substring (Recursion)

--- StringRec.java
+++ StringRec.java
@@ -1,6 +1,6 @@
 public class StringRec {
     public static void main(String[] args){
-        System.out.println(skip1("baccdaha"));
+        System.out.println(skipAppnotApple("bacappcappledaha"));
     }
 
     static void skip(String p, String up){
@@ -28,6 +28,34 @@ public class StringRec {
             return skip1(up.substring(1));
         }else{
             return ch + skip1(up.substring(1));
+        }
+
+    }
+
+    static String skipApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+        if(up.startsWith("apple")){
+           return skipApple(up.substring(5));
+        }else{
+           return ch + skipApple(up.substring(1));
+        }
+
+    }
+
+    static String skipAppnotApple(String up){
+        if(up.isEmpty()){
+            return "";
+        }
+
+        char ch = up.charAt(0);
+        if(up.startsWith("app") && !up.startsWith("apple")){
+           return skipAppnotApple(up.substring(3));
+        }else{
+           return ch + skipAppnotApple(up.substring(1));
         }
 
     }
